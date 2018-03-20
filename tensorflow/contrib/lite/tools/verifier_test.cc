223110b5d232515aa41f23ebfe6d22638b1efc90
A. Unique TensorFlower <gardener@tensorflow.org>
2018-03-19 17:23:20 -0700

    Improve flatbuffer verification.
    PiperOrigin-RevId: 189668634

--- tensorflow/contrib/lite/tools/verifier_test.cc
+++ tensorflow/contrib/lite/tools/verifier_test.cc
@@ -93,8 +93,8 @@ TEST(VerifyModel, TestEmptyModel) {
                            /*description=*/0, /*buffers=*/0);
   ::tflite::FinishModelBuffer(builder, model);
 
-  ASSERT_TRUE(Verify(builder.GetBufferPointer(), builder.GetSize(),
-                     DefaultErrorReporter()));
+  ASSERT_FALSE(Verify(builder.GetBufferPointer(), builder.GetSize(),
+                      MutableOpResolver{}, DefaultErrorReporter()));
 }
 
 TEST(VerifyModel, TestSimpleModel) {
